6c6619ab36133a1e6acc5368ec1682a3e38c84bd
ethnography-ca <amir@ethnography.ca>
2026-02-20 10:13:39 -0800

    Update get-link.js

--- netlify/functions/get-link.js
+++ netlify/functions/get-link.js
@@ -1,1 +1,18 @@
+import { Redis } from '@upstash/redis'
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+})
+
+export default async (req, context) => {
+  // SPOP is an "atomic" operation. It pulls one link out and deletes it instantly.
+  const uniqueLink = await redis.spop("survey_links");
+
+  if (!uniqueLink) {
+    return new Response("Sorry! All survey links have been used.", { status: 404 });
+  }
+
+  // This sends the user to their unique link immediately
+  return Response.redirect(uniqueLink, 302);
+};
